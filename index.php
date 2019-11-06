3ba745dd41c1dcf30d59ac4e79421bb3c53da8e5
smilardic <stjepan.milardic00@gmail.com>
2019-11-07 00:41:16 +0100

    Created a landing page (which is now index.php), and newsfeed page is renamed to newsfeed.php

--- index.php
+++ index.php
@@ -1,177 +1,116 @@
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
   <head>
-    <?php
-      include "includes/head_tag-inc.php";
-    ?>
+    <?php include 'includes/head_tag-inc.php' ?>
     <link rel="icon" href="images/bruhbook_icon.ico" type="image/x-icon">
     <title>Bruhbook</title>
   </head>
 
   <body>
-    <?php include "includes/main_navbar-inc.php"; ?>
-    <div class="container contentContainer">
+    <div class="container-fluid">
+      <div class="row">
 
-      <!-- Post a status container -->
-      <div class="postStatusContainer">
-        <div class="row">
+        <!-- left container -->
+        <div class="col-12 col-md-3 leftContainer">
 
-          <div class="col-12 col-md-9 column_nopadding">
-            <div class="form-group">
-              <textarea class="form-control postStatusTextarea" placeholder="Post a status..." rows="3" maxlength="350"></textarea>
+          <!-- logo (responsive only) -->
+          <div class="responsiveLogo">
+              <img src="images/logo.jpg" class="" alt="">
+              Bruhbook
+          </div>
+
+          <!-- Header -->
+          <div class="leftContainerHeader">
+            Login/register
+          </div>
+
+          <!--  Form -->
+          <div class="logInContainer">
+            <form class="" action="index.html" method="post">
+              <div class="form-group">
+                <label for="usernameInput">Username</label>
+                <input type="text" name="" value="" class="form-control" id="usernameInput">
+              </div>
+              <br>
+
+              <div class="form-group">
+                <label for="usernameInput">Password</label>
+                <input type="text" name="" value="" class="form-control" id="usernameInput">
+              </div>
+
+              <br>
+              <div class="loginWarning">
+                Email or password not correct
+              </div>
+              <br>
+
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary logInBtn">Log in</button>
+              </div>
+
+            </form>
+          </div>
+
+          <div class="createAcc">
+            <div>Don't have an account yet?<br /> <a href="#" class="createAccLink">Click here to register!</a> </div>
+          </div>
+        </div>
+
+
+
+        <!-- right container -->
+        <div class="col-12 col-md-9 rightContainer">
+
+          <!-- da big logo  -->
+          <div class="bruhbookLogo_index">
+            <div class="bbLogo">
+              <img src="images/logo.jpg" class="" alt="">
+            </div>
+            <div class="bruhbookName_index">
+              Bruhbook
             </div>
           </div>
 
-          <div class="col-12 col-md-3 column_nopadding postStatusButtonsDiv">
-            <div class="row postStatusButtons_center">
+          <!-- slogan -->
+          <div class="slogan_index">
+            Something something slogan idk
+          </div>
 
-              <div class="col-4 col-md-12 column_nopadding">
-                <button class="btn btn-danger postStatusButtons" name="">Reset</button>
+          <!-- content -->
+          <div class="content_index">
+            <div class="row">
+              <div class="col-12 col-md-4 imgBox justify-content-end">
+                <img src="images/example_index_img.jpg" class="img-responsive img-thumbnail" alt="">
+                <br>
+                <div class="imgDesc">
+                  Share moments
+                </div>
               </div>
-
-              <div class="col-4 col-md-12 column_nopadding">
-                <button class="btn btn-danger postStatusButtons" name="">Upload image</button>
+              <div class="col-12 col-md-4 imgBox ">
+                <img src="images/example_index_img.jpg" class="img-responsive img-thumbnail" alt="">
+                <br>
+                <div class="imgDesc">
+                  Exchange messages
+                </div>
               </div>
-
-              <div class="col-4 col-md-12  column_nopadding">
-                <button class="btn btn-danger postStatusButtons" name="">Post</button>
+              <div class="col-12 col-md-4 imgBox ">
+                <img src="images/example_index_img.jpg" class="img-responsive img-thumbnail" alt="">
+                <br>
+                <div class="imgDesc">
+                  Like and comment
+                </div>
               </div>
             </div>
-          </div>
 
-        </div>
-      </div>
-
-      <hr>
-      <!-- Header 2 container -->
-      <div class="partHeader">
-        Newsfeed
-      </div>
-
-      <!-- Status container -->
-      <div class="statusContainer">
-        <!-- User who posted a status -->
-        <div class="userPostingContainer">
-
-          <div class="userPostingImg">
-            <img src="images/images.jpg" alt="">
-          </div>
-
-          <div class="userPostingUsername">
-            <span class="color_username newsfeed_username">Person 1</span>
-            &nbsp;
-            <span><i class="fas fa-check-circle"></i></span>
-            &nbsp;
-            <br>
-            <div class="edit_delete_btn_div">
-              <span><i class="fas fa-edit"></i></span>
-              <span><i class="fas fa-trash-alt"></i></span>
+            <!-- footer -->
+            <div class="index_footer">
+              Bruhbook bruh - 2019
+              <br>
+              GitHub: <a href="#">github.com/qwehbrubruhqweqwe</a>
             </div>
-
           </div>
-
         </div>
-
-        <hr>
-
-        <!-- da status -->
-        <div class="statusText">
-          orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskto
-        </div>
-
-        <hr>
-
-        <!-- status buttons -->
-        <div class="statusButtonsContainer">
-          <span class="statusButton likeButton">5 &nbsp; <i class="fas fa-heart"></i></span>
-          <span class="statusButton commentButton">4 &nbsp; <i class="far fa-comment"></i></span>
-        </div>
-
       </div>
-
-
-
-
-
-
-
-      <!-- Status container 2 -->
-      <div class="statusContainer">
-        <!-- User who posted a status -->
-        <div class="userPostingContainer">
-
-          <div class="userPostingImg">
-            <img src="images/logo.jpg" alt="">
-          </div>
-
-          <div class="userPostingUsername">
-            <span class="color_username newsfeed_username">Person 2</span>
-          </div>
-
-        </div>
-
-        <hr>
-
-        <!-- da status -->
-        <div class="statusText">
-          orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-        </div>
-
-        <hr>
-
-        <!-- status buttons -->
-        <div class="statusButtonsContainer">
-          <span class="statusButton likeButton">1 &nbsp; <i class="fas fa-heart"></i></span>
-          <span class="statusButton commentButton">0 &nbsp; <i class="far fa-comment"></i></span>
-        </div>
-
-      </div>
-
-
-
-
-
-
-
-      <!-- Status container 2 -->
-      <div class="statusContainer">
-        <!-- User who posted a status -->
-        <div class="userPostingContainer">
-
-          <div class="userPostingImg">
-            <img src="images/logo.jpg" alt="">
-          </div>
-
-          <div class="userPostingUsername">
-            <span class="color_username newsfeed_username">Person 3</span>
-            &nbsp;
-            <span><i class="fas fa-check-circle"></i></span>
-            &nbsp;
-          </div>
-
-        </div>
-
-        <hr>
-
-        <!-- da status -->
-        <div class="statusText">
-          orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-        </div>
-
-        <hr>
-
-        <!-- status buttons -->
-        <div class="statusButtonsContainer">
-          <span class="statusButton likeButton">48 &nbsp; <i class="fas fa-heart"></i></span>
-          <span class="statusButton commentButton">14 &nbsp; <i class="far fa-comment"></i></span>
-        </div>
-
-      </div> <!-- end of status -->
-
-      <div class="loadMoreDiv">
-        Load more
-      </div>
-
     </div>
   </body>
 </html>
